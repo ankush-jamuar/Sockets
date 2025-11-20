497a54a67ee3195e7ca4cf1f06a3cd97a700a880
ankush <jamuar66@gmail.com>
2025-11-20 11:21:32 +0530

    welcome for newe and count for broadcast

---
+++ broadcast1.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import {createServer} from 'http'
+import {Server} from 'socket.io'
+
+const app=express()
+const http = createServer(app)
+const io=new Server(http)
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, "broadcast1.html"))
+})
+
+let clients = 0;
+
+io.on("connection", (socket)=>{
+    socket.emit("broadcast", "Welcome")
+    clients++;
+    socket.broadcast.emit("broadcast", `${clients} clients connected.`)
+    socket.on("disconnect", ()=>{
+        clients--;
+        io.emit("broadcast",`${clients} clients connected`)
+    })
+})
+
+http.listen(3000)
