f1da02ae2d16300b98c6cb3cf7aea4e835c36e46
ankush <jamuar66@gmail.com>
2025-12-18 11:56:54 +0530

    some problem

--- chatserver.js
+++ chatserver.js
@@ -9,13 +9,23 @@ const io = new Server(http);
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import pg from 'pg';
+const { Pool } = pg;
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'chattywatty',
+    password: 'Ank@1234',
+    port: 5432
+})
 
-import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost:27017/chattywatty");
-const chatSchema = new mongoose.Schema({
-  messages: String,
-});
-const Chat = mongoose.model("mesaages", chatSchema);
+
+// import mongoose from "mongoose";
+// mongoose.connect("mongodb://localhost:27017/chattywatty");
+// const chatSchema = new mongoose.Schema({
+//   messages: String,
+// });
+// const Chat = mongoose.model("mesaages", chatSchema);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -26,13 +36,13 @@ app.get("/", (req, res) => {
 
 io.on("connection", async(socket) => {
   console.log("A client is connected: ", socket.id);
-  const chats =  await Chat.find();
-  chats.forEach((chat)=>{
-    socket.emit("chatmessage", chat.messages);
-  })
+//   const chats =  await Chat.find();
+//   chats.forEach((chat)=>{
+//     socket.emit("chatmessage", chat.messages);
+//   })
   socket.on("chatmessage", async(msg) => {
     io.emit("chatmessage", msg);
-    await Chat.insertOne({messages: msg})
+    await pool.query
   });
   socket.on("disconnect", () => {
     console.log("A client is disconnected: ", socket.id);
