1b5784eb4f246c552d5f5bc06c11e2fc8de11e6c
ankush <jamuar66@gmail.com>
2025-12-18 11:51:33 +0530

    made changes to the chatapp and made it very scary

--- chatserver.js
+++ chatserver.js
@@ -1,30 +1,42 @@
-import express from 'express'
-import {createServer} from 'http'
-import {Server} from 'socket.io'
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const app=express()
-const http = createServer(app)
-const io=new Server(http)
+const app = express();
+const http = createServer(app);
+const io = new Server(http);
 
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+import mongoose from "mongoose";
+mongoose.connect("mongodb://localhost:27017/chattywatty");
+const chatSchema = new mongoose.Schema({
+  messages: String,
+});
+const Chat = mongoose.model("mesaages", chatSchema);
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, "chatclient.html"))
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-io.on("connection", (socket)=>{
-    console.log("A client is connected: ", socket.id)
-    socket.on("chatmessage", (msg)=>{
-        io.emit("chatmessage", msg)
-    })
-    socket.on("disconnect", ()=>{
-        console.log("A client is disconnected: ",socket.id)
-    })
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "chatclient.html"));
+});
 
-http.listen(3000)
+io.on("connection", async(socket) => {
+  console.log("A client is connected: ", socket.id);
+  const chats =  await Chat.find();
+  chats.forEach((chat)=>{
+    socket.emit("chatmessage", chat.messages);
+  })
+  socket.on("chatmessage", async(msg) => {
+    io.emit("chatmessage", msg);
+    await Chat.insertOne({messages: msg})
+  });
+  socket.on("disconnect", () => {
+    console.log("A client is disconnected: ", socket.id);
+  });
+});
+
+http.listen(3000);
